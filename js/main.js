4b8383f0c62fcca9892fdc5b67b1438037beeb5d
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-09-02 15:12:18 -0400

    fixed prettier

--- js/main.js
+++ js/main.js
@@ -1,150 +1,175 @@
 (function () {
-    'use strict';
-    var isMobile = {
-        Android: function () {
-            return navigator.userAgent.match(/Android/i);
-        }
-        , BlackBerry: function () {
-            return navigator.userAgent.match(/BlackBerry/i);
-        }
-        , iOS: function () {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        }
-        , Opera: function () {
-            return navigator.userAgent.match(/Opera Mini/i);
-        }
-        , Windows: function () {
-            return navigator.userAgent.match(/IEMobile/i);
-        }
-        , any: function () {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-    };
-    // Full Height
-    var fullHeight = function () {
-        if (!isMobile.any()) {
-            $('.js-fullheight').css('height', $(window).height());
-            $(window).resize(function () {
-                $('.js-fullheight').css('height', $(window).height());
-            });
-        }
-    };
-    // Animations
-    var contentWayPoint = function () {
-        var i = 0;
-        $('.animate-box').waypoint(function (direction) {
-            if (direction === 'down' && !$(this.element).hasClass('animated')) {
-                i++;
-                $(this.element).addClass('item-animate');
-                setTimeout(function () {
-                    $('body .animate-box.item-animate').each(function (k) {
-                        var el = $(this);
-                        setTimeout(function () {
-                            var effect = el.data('animate-effect');
-                            if (effect === 'fadeIn') {
-                                el.addClass('fadeIn animated');
-                            }
-                            else if (effect === 'fadeInLeft') {
-                                el.addClass('fadeInLeft animated');
-                            }
-                            else if (effect === 'fadeInRight') {
-                                el.addClass('fadeInRight animated');
-                            }
-                            else {
-                                el.addClass('fadeInUp animated');
-                            }
-                            el.removeClass('item-animate');
-                        }, k * 200, 'easeInOutExpo');
-                    });
-                }, 100);
-            }
-        }, {
-            offset: '85%'
-        });
-    };
-    var wind = $(window);
-    // Navbar scrolling background
-    wind.on("scroll", function () {
-        var bodyScroll = wind.scrollTop()
-            , navbar = $(".navbar")
-            , logo = $(".navbar .logo> img");
-        if (bodyScroll > 100) {
-            navbar.addClass("nav-scroll");
-            logo.attr('src', 'images/TE_logo.png');
-        }
-        else {
-            navbar.removeClass("nav-scroll");
-            logo.attr('src', 'images/TE_logo.png');
-        }
-    });
-    // Close navbar-collapse when a  clicked
-    $(".navbar-nav a").on('click', function () {
-        $(".navbar-collapse").removeClass("show");
-    });
-    // Slider
-    var sliderMain = function () {
-        $('.addo-hero .flexslider').flexslider({
-            animation: "fade"
-            , slideshowSpeed: 5000
-            , directionNav: true
-            , start: function () {
-                setTimeout(function () {
-                    $('.slider-text').removeClass('animated fadeInUp');
-                    $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-                }, 500);
-            }
-            , before: function () {
-                setTimeout(function () {
-                    $('.slider-text').removeClass('animated fadeInUp');
-                    $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-                }, 500);
-            }
-        });
-    };
-    // Document on load.
-    $(function () {
-        fullHeight();
-        contentWayPoint();
-        sliderMain();
-    });
-    // Smooth Scrolling
-    $('a[href*="#"]')
-        // Remove links that don't actually link to anything
-        .not('[href="#"]').not('[href="#0"]').click(function (event) {
-            // On-page links
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                // Figure out element to scroll to
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000, function () {
-                        // Callback after animation
-                        // Must change focus!
-                        var $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) { // Checking if the target was focused
-                            return false;
-                        }
-                        else {
-                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                            $target.focus(); // Set focus again
-                        };
-                    });
-                }
-            }
-        });
-    // Button
-    var buttons = document.querySelectorAll(".toolbar .btn");
-    for (var i = 0; i < buttons.length; i++) {
-        var button = buttons[i];
-        button.addEventListener("click", function () {
-            if (!button.classList.contains("active")) button.classList.add("active");
-            else button.classList.remove("active");
-        });
+  'use strict';
+  var isMobile = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+      return (
+        isMobile.Android() ||
+        isMobile.BlackBerry() ||
+        isMobile.iOS() ||
+        isMobile.Opera() ||
+        isMobile.Windows()
+      );
+    },
+  };
+  // Full Height
+  var fullHeight = function () {
+    if (!isMobile.any()) {
+      $('.js-fullheight').css('height', $(window).height());
+      $(window).resize(function () {
+        $('.js-fullheight').css('height', $(window).height());
+      });
     }
-}());
+  };
+  // Animations
+  var contentWayPoint = function () {
+    var i = 0;
+    $('.animate-box').waypoint(
+      function (direction) {
+        if (direction === 'down' && !$(this.element).hasClass('animated')) {
+          i++;
+          $(this.element).addClass('item-animate');
+          setTimeout(function () {
+            $('body .animate-box.item-animate').each(function (k) {
+              var el = $(this);
+              setTimeout(
+                function () {
+                  var effect = el.data('animate-effect');
+                  if (effect === 'fadeIn') {
+                    el.addClass('fadeIn animated');
+                  } else if (effect === 'fadeInLeft') {
+                    el.addClass('fadeInLeft animated');
+                  } else if (effect === 'fadeInRight') {
+                    el.addClass('fadeInRight animated');
+                  } else {
+                    el.addClass('fadeInUp animated');
+                  }
+                  el.removeClass('item-animate');
+                },
+                k * 200,
+                'easeInOutExpo',
+              );
+            });
+          }, 100);
+        }
+      },
+      {
+        offset: '85%',
+      },
+    );
+  };
+  var wind = $(window);
+  // Navbar scrolling background
+  wind.on('scroll', function () {
+    var bodyScroll = wind.scrollTop(),
+      navbar = $('.navbar'),
+      logo = $('.navbar .logo> img');
+    if (bodyScroll > 100) {
+      navbar.addClass('nav-scroll');
+      logo.attr('src', 'images/TE_logo.png');
+    } else {
+      navbar.removeClass('nav-scroll');
+      logo.attr('src', 'images/TE_logo.png');
+    }
+  });
+  // Close navbar-collapse when a  clicked
+  $('.navbar-nav a').on('click', function () {
+    $('.navbar-collapse').removeClass('show');
+  });
+  // Slider
+  var sliderMain = function () {
+    $('.addo-hero .flexslider').flexslider({
+      animation: 'fade',
+      slideshowSpeed: 5000,
+      directionNav: true,
+      start: function () {
+        setTimeout(function () {
+          $('.slider-text').removeClass('animated fadeInUp');
+          $('.flex-active-slide')
+            .find('.slider-text')
+            .addClass('animated fadeInUp');
+        }, 500);
+      },
+      before: function () {
+        setTimeout(function () {
+          $('.slider-text').removeClass('animated fadeInUp');
+          $('.flex-active-slide')
+            .find('.slider-text')
+            .addClass('animated fadeInUp');
+        }, 500);
+      },
+    });
+  };
+  // Document on load.
+  $(function () {
+    fullHeight();
+    contentWayPoint();
+    sliderMain();
+  });
+  // Smooth Scrolling
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(':focus')) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            },
+          );
+        }
+      }
+    });
+  // Button
+  var buttons = document.querySelectorAll('.toolbar .btn');
+  for (var i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    button.addEventListener('click', function () {
+      if (!button.classList.contains('active')) button.classList.add('active');
+      else button.classList.remove('active');
+    });
+  }
+})();
