12202fa5f9e0331d17d1bd4c160849697b447357
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-09-03 19:59:05 -0400

    added puplic folder

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'index.html')
+        index: resolve(__dirname, 'index.html'),
       },
     },
   },
